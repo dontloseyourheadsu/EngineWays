014740d676a001a4be1d2570179d398b83195227
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2026-05-16 21:50:44 -0600

    Added Mapsui for avalonia

--- frontend/EngineWays.Frontend/Views/MainWindow.axaml.cs
+++ frontend/EngineWays.Frontend/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Mapsui;
+using Mapsui.Tiling;
+using Mapsui.Extensions;
 
 namespace EngineWays.Frontend.Views;
 
@@ -7,5 +10,20 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        InitializeMap();
+    }
+
+    private void InitializeMap()
+    {
+        MapControl.Map.Layers.Add(OpenStreetMap.CreateTileLayer());
+        
+        // Center on Mexico City
+        var centerOfCdmx = new MPoint(-99.1332, 19.4326);
+        var pos = Mapsui.Projections.SphericalMercator.FromLonLat(centerOfCdmx.X, centerOfCdmx.Y);
+        var sphericalMercatorCoordinate = new MPoint(pos.x, pos.y);
+        
+        // Use the navigator to set the initial view
+        MapControl.Map.Navigator.CenterOn(sphericalMercatorCoordinate);
+        MapControl.Map.Navigator.ZoomToLevel(10);
     }
 }
