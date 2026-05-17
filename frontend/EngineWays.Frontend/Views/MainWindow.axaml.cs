bba09a0459f33bcac798b9107645824dd8952a5e
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2026-05-16 19:34:04 -0600

    Start frontend project

---
+++ frontend/EngineWays.Frontend/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace EngineWays.Frontend.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
